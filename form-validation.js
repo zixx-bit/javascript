4d91da2fac6e971f7787296348819178c727dc71
zixx-bit <65281903+zixx-bit@users.noreply.github.com>
2023-08-04 16:54:42 +0300

    form validation using javascript

---
+++ form-validation.js
@@ -0,0 +1,14 @@
+const myform = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myform.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefaul();
+
+    if
+    
+}
